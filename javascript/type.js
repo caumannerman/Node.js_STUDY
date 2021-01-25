fb512bd8bb69ba93008b8558db5eda38b41b06ee
Caumannerman <uigohiiming2@gmail.com>
2021-01-25 17:41:57 +0900

    Module usage

---
+++ javascript/type.js
@@ -0,0 +1,12 @@
+const {sum, sub} = require('./expo');
+
+console.log(sum(2,4));
+console.log(sub(5,2));
+
+function run(){
+    console.log("3chgh");
+}
+
+console.log("start");
+setTimeout(run,0);
+console.log("end");
