b9136a3bcf6599a5f026f8a27d9e7881726f7e88
Caumannerman <uigohiiming2@gmail.com>
2021-02-08 16:11:50 +0900

    FirstRestfulServer

--- FirstRestfulServer/restServer.js
+++ FirstRestfulServer/restServer.js
@@ -8,6 +8,7 @@ http.createServer((req, res) => {
   if (req.method === 'GET') {
 
     if (req.url === '/') {
+      console.log(req.url);
       return fs.readFile('./restFront.html', (err, data) => {
         if (err) {
           throw err;
@@ -17,6 +18,7 @@ http.createServer((req, res) => {
     } 
     
     else if (req.url === '/about') {
+      console.log(req.url);
       return fs.readFile('./about.html', (err, data) => {
         if (err) {
           throw err;
@@ -43,14 +45,18 @@ http.createServer((req, res) => {
   else if (req.method === 'POST') {
     if (req.url === '/users') {
       let body = '';
-      req.on('data', (data) => {
+      req.on('data', (data) => {// readStream의 이벤트처럼.
         body += data;
       });
       return req.on('end', () => {
         console.log('POST 본문(Body):', body);
-        const { name } = JSON.parse(body);
+        const { name } = JSON.parse(body);  // 문자열을 JSON형태로
+        console.log(name);
+        console.log(typeof name);
         const id = +new Date();
+        console.log(typeof id);
         users[id] = name;
+
         res.writeHead(201);
         res.end('등록 성공');
       });
@@ -58,18 +64,22 @@ http.createServer((req, res) => {
   }
   
   
-  else if (req.method === 'PUT') {
+  else if (req.method === 'PUT') { // 수정버튼 눌렀을 때.
+
     if (req.url.startsWith('/users/')) {
+      console.log(req.url);
       const key = req.url.split('/')[2];
       let body = '';
       req.on('data', (data) => {
         body += data;
       });
+      
       return req.on('end', () => {
         console.log('PUT 본문(Body):', body);
         users[key] = JSON.parse(body).name;
         return res.end(JSON.stringify(users));
       });
+      
     }
   }
   
@@ -90,7 +100,4 @@ http.createServer((req, res) => {
   .listen(8085, () => {
     console.log('8085번 포트에서 서버 대기중입니다');
   });
-
-
-
 
